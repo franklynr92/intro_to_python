e5042acc2c31259e16663dfea5f41a32ace9460e
Franklyn Rodriguez <franklynr92@gmail.com>
2020-11-02 15:59:29 -0500

    simple functions

---
+++ simple_func.py
@@ -0,0 +1,13 @@
+#Function definition
+def greetingEnglish():
+    #this function greets user in Enlgish language.
+    print("Greetings and Salutations")
+    return
+
+def greetingSpanish():
+    #this function greets user in Spanish language).
+    print("Buenos Dias")
+    return
+
+greetingEnglish()
+greetingSpanish()
