c01cdc49a425dadcd2b8d0239de507362ced3758
Franklyn Rodriguez <franklynr92@gmail.com>
2020-11-02 16:21:45 -0500

    learning about the aestriks dtreating what is next to it as a tupple

--- employeeFunc.py
+++ employeeFunc.py
@@ -4,4 +4,12 @@ def employeeInformation(name="", ssn="000-000-000", position=""):
     print("Position:", position)
     return
 
+def moreEmployee(name, *other):
+    print("Employee Info:")
+    print("Name:", name)
+    for var in other:
+        print(var)
+    return
+
 employeeInformation(name="Franklyn", ssn="000-000-000", position="CEO")
+moreEmployee("Mark Lassoff", "Founder", "9-1-2009")
