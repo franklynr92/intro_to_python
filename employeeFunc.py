de4c79fbc38449eb5f117e9e8d85e4bf755c4eec
Franklyn Rodriguez <franklynr92@gmail.com>
2020-11-02 16:18:22 -0500

    employeefunc"

---
+++ employeeFunc.py
@@ -0,0 +1,7 @@
+def employeeInformation(name, ssn, position):
+    print("Name:", name)
+    print("Ssn:", ssn)
+    print("Position:", position)
+    return
+
+employeeInformation(name="Franklyn", ssn="000-000-000", position="CEO")
